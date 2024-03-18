05120634a2ff6fec0d2f06bbf02c4210dc5808cc
majdmsallam <msalm46@gmail.com>
2024-03-18 19:59:30 +0200

    display fixes

--- src/TopBar/Topbar.js
+++ src/TopBar/Topbar.js
@@ -1,47 +1,10 @@
-// import React from "react";
-// import classes from './Topbar.module.css'
-// import MajdProfileImage from './images/MajdProfile.png';
-// import AnchorLink from "react-anchor-link-smooth-scroll";
-// const Topbar = () => {
-//     return (
-//         <header>
-//             <nav className={classes.Topbar}>
-//                 <div className={classes.start}>
-//                     <img src={MajdProfileImage} alt="Majd Profile img" />
-//                     <span>MAJD MSALLAM</span>
-//                 </div>
-//                 <div className={classes.end}>
-//                     <ul>
-//                         <AnchorLink href="#Home">
-//                             <li>HOME</li>
-//                         </AnchorLink>
-//                         <AnchorLink href="#About">
-//                             <li>ABOUT</li>
-//                         </AnchorLink>
-//                         <AnchorLink href="#Projects">
-//                             <li>PROJECTS</li>
-//                         </AnchorLink>
-//                         <AnchorLink href="Contact">
-//                             <li>CONTACT</li>
-//                         </AnchorLink>
-
-//                     </ul>
-//                 </div>
-//             </nav>
-
-//         </header>
-//     );
-// }
-
-// export default Topbar;
-
 import React, { useState } from "react";
 import classes from './Topbar.module.css'
 import MajdProfileImage from './images/MajdProfile.png';
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const Topbar = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(true);
 
     const toggleMenu = () => {
         setMenuOpen(!menuOpen);
